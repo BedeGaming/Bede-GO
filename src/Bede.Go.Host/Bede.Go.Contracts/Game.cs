8aac52d8dd973f7556e7e6231c5cfd1cbd0d7614
Daniel Lackenby <daniel.lackenby@bedegaming.com>
2017-12-07 10:20:06 +0000

    Define Game and Location

---
+++ src/Bede.Go.Host/Bede.Go.Contracts/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Common;
+
+namespace Bede.Go.Contracts
+{
+    public class Game
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal Distance { get; set; }
+        public DateTime StartTime { get; set; }
+        public Location Location { get; set; }
+    }
+}
